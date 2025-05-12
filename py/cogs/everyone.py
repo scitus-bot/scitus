4c1b25d58256175d098b68f37fb6e0fcb97a7c49
nathan-tat <nathan.tatkowski@outlook.com>
2025-05-12 18:46:39 +0100

    Moved LaTeX command to latex cog

--- py/cogs/everyone.py
+++ py/cogs/everyone.py
@@ -1,15 +1,10 @@
 from typing import Optional
-import os
-from pdf2image import convert_from_path
-from PIL import Image
-from time import time
-
 
 import discord
 from discord import app_commands
 from discord.ext import commands
 
-from pasta import file_to_dict, pdf_to_image, DATA
+from pasta import file_to_dict, DATA
 
 data = DATA
 channels: dict = file_to_dict(data + "channels.json")
@@ -157,74 +152,6 @@ class Everyone(commands.Cog):
     async def buttoncommand(self, inter: discord.Interaction) -> None:
         await inter.response.send_message("button", view=MyView())
 
-
-#--------------------------------------------------------------------------------------------------------------------------
-    # LaTeX
-
-
-    @app_commands.command(
-        name="latex",
-        description="Generates an image using LaTeX from a given prompt that uses LaTeX syntax."
-    )
-    async def latex(self, inter: discord.Interaction, prompt: str) -> None:
-        """ Convert a text prompt to a generated LaTeX file """
-        await inter.response.send_message("Processing....")
-        
-        # Generates a unique file name (in case multiple being processed at the same time)
-        fname: str = str(inter.id)
-        
-        
-        # Writing to the latex file
-        with open(f"{fname}.tex", "w") as l:
-            l.write(
-                ("\\documentclass[border={2pt, 2pt, 2pt, 2pt}]{standalone}\n"
-                 "\\usepackage{amssymb}\n"
-                "\\begin{document}\n"
-                "$\\displaystyle\n")
-            )
-            l.write(prompt)
-            l.write(
-                ("\n$\n\\end{document}")
-            )
-            
-        
-        # Compiling latex to pdf
-        os.system(f"pdflatex -quiet {fname}.tex")
-        
-        
-        # Converting pdf to an image
-        images: list[Image.Image] = convert_from_path(f"{fname}.pdf", 1000)
-        images[0].save(f"image{fname}.jpg", "JPEG")
-        
-        # Cropping and sending the generated image
-        # im = Image.open(f"image{fname}.jpg")
-        # im1 = im.crop((0, 1240, 4133, 3446))
-        
-        # im1.save(f"crop{fname}.jpg")
-        
-        
-        embed: discord.Embed = discord.Embed(
-            title="LaTeX output",
-            color=0xEEDB83,
-            description=prompt
-        )
-        file = discord.File(f"image{fname}.jpg", filename=f"image{fname}.jpg")
-        embed.set_image(url=f"attachment://image{fname}.jpg")
-        await inter.channel.send(
-            content=None,
-            file=file,
-            embed=embed   
-        )
-        await inter.delete_original_response()
-        
-        # Removing all the files made
-        os.remove(f"{fname}.aux")
-        os.remove(f"{fname}.log")
-        os.remove(f"{fname}.pdf")
-        os.remove(f"{fname}.tex")
-        os.remove(f"image{fname}.jpg")
-        # os.remove(f"crop{fname}.jpg")
-        
 
     
 async def setup(bot: commands.Bot) -> None:
